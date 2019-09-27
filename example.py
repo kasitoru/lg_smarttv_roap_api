db10ba1b015f415a15feae65c955dbfa6dade1a2
Sergey Avdeev <avdeevsv91@gmail.com>
2019-09-27 11:47:15 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,48 @@
+# LG SmartTV ROAP API
+# Author: Sergey Avdeev
+# E-Mail: avdeevsv91@gmail.com
+# URL: https://github.com/avdeevsv91/lg_smarttv_roap_api
+
+import cv2, numpy
+from LG import SmartTV
+
+IP_ADDRESS = '192.168.0.198'
+TV_KEY = ''
+
+# Find TV
+while(not IP_ADDRESS):
+	print('Searching TV...')
+	IP_ADDRESS = SmartTV.findTV()
+print('TV IP: ' + IP_ADDRESS)
+TV = SmartTV(IP_ADDRESS)
+
+# Authorization
+auth = False
+print('Authorization...')
+while(not auth):
+	if TV_KEY:
+		try:
+			TV.sendKey(TV_KEY)
+			auth = True
+		except:
+			print('Wrong key! Try again...')
+			TV_KEY = ''
+	else:
+		try:
+			TV.displayKey()
+			TV_KEY = input('Enter key: ')
+		except:
+			print('TV is offline!')
+
+# Send simple command
+TV.sendCommand(SmartTV.TV_CMD_CHANNEL_UP)
+
+# Screen capture
+print('Start capturing...')
+while(True):
+	image = TV.getData(SmartTV.TV_INFO_SCREEN)
+	npimg = numpy.frombuffer(image, numpy.uint8)
+	frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+	cv2.imshow('TV ({0}) [Press ESC to exit]'.format(IP_ADDRESS), frame)
+	if cv2.waitKey(1) == 27:
+		break
